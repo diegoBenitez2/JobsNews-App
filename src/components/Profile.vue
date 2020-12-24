502453e92421f5e4fe502168f8d38b1d636ce434
skorpion <diegof.17@hotmail.com>
2020-12-24 13:42:00 -0500

    creating babges jobs

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -6,6 +6,10 @@
       <div class="description">
         <div class="states-company">
           <p class="company">{{ profile.company }}</p>
+          <p v-if="profile.new"
+          class="new ">new!</p>
+          <p  v-if="profile.featured"
+          class="featured">featured</p>
         </div>
         <h3 class="position">{{ profile.position }}</h3>
         <div class="features">
@@ -13,9 +17,22 @@
           <p class="desc-features">{{ profile.contract }}</p>
           <p class="desc-features">{{ profile.location }}</p>
         </div>
-        <div class="roles-skill"></div>
       </div>
     </div>
+        <div class="roles-skills">
+          <button class="btn-skills">{{profile.role}}</button
+          ><button class="btn-skills">{{profile.level}}</button>
+          <button v-for="language in profile.languages"
+                :key=language
+            class=" btn-skills languages">{{language}}</button>
+            <button 
+            class="btn-skills tools"
+            v-for="tool in profile.tools"
+            :key="tool"
+            >
+            {{tool}}
+            </button>
+        </div>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,7 +19,7 @@ export default {
   },
   data() {
     return {
-      profiles: [],
+      profiles: [], 
     };
   },
   mounted: function() {
